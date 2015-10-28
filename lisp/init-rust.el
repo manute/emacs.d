dc42694f235893ddb8e92b2a7cef73a3f59f10ba
Manuel Alonso <manuteali@gmail.com>
2015-10-28 23:49:19 +0100

    Fixing rust setup

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,8 +1,3 @@
-
-;; Init Rust
-;; -----------------------------------
-;; http://bassam.co/emacs/2015/08/24/rust-with-emacs/
-
 (require 'rust-mode)
 (require 'racer)
 (require 'company-racer)
@@ -10,31 +5,17 @@
 
 
 ; Set path to racer binary
-(setq racer-cmd "~/.racer/target/release/racer")
+(setq racer-cmd "/usr/local/bin/racer")
 
 ;; Set path to rust src directory
-(setq racer-rust-src-path "~/.rust/src/")
+(setq racer-rust-src-path "/usr/local/src/rust/src")
 
-;; Setting up configurations when you load rust-mode
-(add-hook 'rust-mode-hook
+(add-hook 'rust-mode-hook #'racer-mode)
 
-          '(lambda ()
-             ;; Enable racer
-             (racer-activate)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
 
-             ;; Hook in racer with eldoc to provide documentation
-             (racer-turn-on-eldoc)
-
-             ;; Use flycheck-rust in rust-mode
-             (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-
-             ;; Use company-racer in rust mode
-             (set (make-local-variable 'company-backends) '(company-racer))
-
-             ;; Key binding to jump to method definition
-             (local-set-key (kbd "M-.") #'racer-find-definition)))
-
-             ;; Rustfmt befor save
-;;           (add-hook 'before-save-hook #'rustfmt-format-buffer)))
+(global-set-key (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
 
 (provide 'init-rust)
