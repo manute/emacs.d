e13ddb2b162c765244ba7864697d930e2a283f29
malonso <manuel.alonso@corp.ovh.com>
2016-01-26 18:15:17 +0100

    Changing racket mode

--- lisp/init-racket.el
+++ lisp/init-racket.el
@@ -1,10 +1,22 @@
 (require 'racket-mode)
 
-;(setq racket-racket-program /usr/local/bin/racket)
-;(setq racket-raco-program /usr/local/bin/raco)
+
+;; (setq racket-racket-program /home/manu/racket/bin/racket)
+;; (setq racket-raco-program /home/manu/racket/bin/raco)
+
+(setq tab-always-indent 'complete)
+
+;; Bug exists in Racket company backend that opens docs in new window when
+;; company-quickhelp calls it. Note hook is appendended for proper ordering.
 
 (add-hook 'racket-mode-hook
           (lambda ()
-            (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
+            (define-key racket-mode-map (kbd "C-c r") 'racket-run)
+            (company-quickhelp-mode -1)))
+
+(add-hook 'racket-repl-mode-hook
+          (lambda ()
+            (company-quickhelp-mode -1)))
+
 
 (provide 'init-racket)
