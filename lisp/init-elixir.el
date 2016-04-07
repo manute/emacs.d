545cf0c80afcf35335d72d48eb6449392c5e1cad
manute <manuel.alonso@protonmail.com>
2016-04-07 10:40:17 +0200

    Adding more config to elixir mode

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -1,19 +1,37 @@
+
+(defun manu/my-elixir-do-end-close-action (id action context)
+  (when (eq action 'insert)
+    (newline-and-indent)
+    (forward-line -1)
+    (indent-according-to-mode)))
+
+
 (use-package elixir-mode
   :ensure t
   :mode (("\\.ex[s]\\'" . elixir-mode)
          ("\\.elixir[2]\\'" . elixir-mode))
   :config
-  (add-to-list 'elixir-mode-hook
-             (defun auto-activate-ruby-end-mode-for-elixir-mode ()
-               (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
-                    "\\(?:^\\|\\s-+\\)\\(?:do\\)")
-               (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
-               (ruby-end-mode +1)))
 
   (add-hook 'elixir-mode-hook #'company-mode)
 
   ;; NO FLYCHECK YET
   ;; (add-hook 'elixir-mode-hook #'flycheck-mode)
+
+  (use-package smartparens
+    :ensure t
+    :config
+
+    (sp-with-modes '(elixir-mode)
+      (sp-local-pair "->" "end"
+                     :when '(("RET"))
+                     :post-handlers '(:add my-elixir-do-end-close-action)
+                     :actions '(insert)))
+
+    (sp-with-modes '(elixir-mode)
+      (sp-local-pair "do" "end"
+                     :when '(("SPC" "RET"))
+                     :post-handlers '(:add my-elixir-do-end-close-action)
+                     :actions '(insert))))
 
   (use-package alchemist
     :ensure t
