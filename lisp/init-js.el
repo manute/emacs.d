bfe037b6b3ac9cb595082e91edc4690bbf9451aa
manute <manuel.alonso@protonmail.com>
2016-04-22 15:05:26 +0200

    Activating eslint for js2-mode and documenting

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,15 +1,20 @@
+
+;; npm install -g eslint babel-eslint eslint-plugin-react
+;; file ~/.eslintrc with rules for this url -> ;; http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+
 (use-package js2-mode
   :ensure t
   :mode ("\\.js\\'" . js2-mode)
   :config
 
+  (add-hook 'js2-mode-hook   #'company-mode)
+
+  (add-hook 'js2-mode-hook   #'flycheck-mode)
+  (setq flycheck-disable-checker '(javascript-jshint))
+
   (use-package smartparens
     :ensure t
-    :config  (add-hook 'js2-mode-hook 'smartparens-mode))
+    :config  (add-hook 'js2-mode-hook #'smartparens-mode)))
 
-  (use-package company-go
-    :ensure t
-    :config
-    (add-hook 'js2-mode-hook 'company-mode)))
 
 (provide 'init-js)
