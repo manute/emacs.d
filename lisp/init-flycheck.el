1ab9641f7c847d799e1d03d8abff8c8e87e68003
Manuel Alonso <manuteali@gmail.com>
2016-05-29 18:46:45 +0200

    configuring flycheck with :after command

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -7,22 +7,23 @@
 
   ;; Jump between current errors with M-n and M-p.
   (global-set-key (kbd "M-n") 'next-error)
-  (global-set-key (kbd "M-p") 'previous-error)
+  (global-set-key (kbd "M-p") 'previous-error))
 
-  (use-package flycheck-color-mode-line
-    :ensure t
-    :config
-    (setq flycheck-highlighting-mode 'symbols)
-    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
-    (set-face-background 'flycheck-error "#660000")
-    (set-face-foreground 'flycheck-error nil)
-    (set-face-background 'flycheck-warning "#331800")
-    (set-face-foreground 'flycheck-warning nil)
-    (set-face-background 'flycheck-color-mode-line-error-face "#440000")
-    (set-face-background 'flycheck-color-mode-line-warning-face "#553300")
-    (set-face-background 'flycheck-color-mode-line-info-face nil)
-    (set-face-foreground 'flycheck-color-mode-line-error-face "#ffffff")
-    (set-face-foreground 'flycheck-color-mode-line-warning-face "#ffffff")
-    (set-face-foreground 'flycheck-color-mode-line-info-face nil)))
+(use-package flycheck-color-mode-line
+  :ensure t
+  :after flycheck
+  :config
+  (setq flycheck-highlighting-mode 'symbols)
+  (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
+  (set-face-background 'flycheck-error "#660000")
+  (set-face-foreground 'flycheck-error nil)
+  (set-face-background 'flycheck-warning "#331800")
+  (set-face-foreground 'flycheck-warning nil)
+  (set-face-background 'flycheck-color-mode-line-error-face "#440000")
+  (set-face-background 'flycheck-color-mode-line-warning-face "#553300")
+  (set-face-background 'flycheck-color-mode-line-info-face nil)
+  (set-face-foreground 'flycheck-color-mode-line-error-face "#ffffff")
+  (set-face-foreground 'flycheck-color-mode-line-warning-face "#ffffff")
+  (set-face-foreground 'flycheck-color-mode-line-info-face nil))
 
 (provide 'init-flycheck)
