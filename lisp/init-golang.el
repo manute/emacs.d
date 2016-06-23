fd45f28ba7e1277e3842c0e2be7e365f747a582d
manute <manuel.alonso@protonmail.com>
2016-06-23 11:05:35 +0200

    configuring packages wwith after mode

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -10,14 +10,6 @@
   :mode ("\\.go\\'" . go-mode)
   :config
 
-  (use-package exec-path-from-shell
-    :ensure t
-    :if (memq window-system '(mac ns))
-    :config
-    (exec-path-from-shell-initialize)
-    (exec-path-from-shell-copy-env "GOROOT")
-    (exec-path-from-shell-copy-env "GOPATH"))
-
   ;; (when (memq window-system '(mac ns))
   ;;   (setenv "PATH" (concat (getenv "PATH") (getenv "GOPATH")))
   ;;   (setenv "PATH" (concat (getenv "PATH") (getenv "GOROOT"))))
@@ -25,24 +17,37 @@
   (setq compile-command "go build -v && go test -v && go vet")
   (setq gofmt-command "goimports")
   (add-hook 'go-mode-hook #'smartparens-mode)
-  (add-hook 'before-save-hook #'gofmt-before-save)
+  (add-hook 'before-save-hook #'gofmt-before-save))
 
-  (use-package company-go
-    :ensure t
-    :config
-    (add-hook 'go-mode-hook 'company-mode)
-    (add-hook 'go-mode-hook (lambda ()
-                              (set (make-local-variable 'company-backends) '(company-go))
-                              (company-mode))))
+(use-package exec-path-from-shell
+  :ensure t
+  :if (memq window-system '(mac ns))
+  :after go-mode
+  :config
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOROOT")
+  (exec-path-from-shell-copy-env "GOPATH"))
 
-  (use-package go-eldoc
-    :ensure t
-    :diminish eldoc-mode
-    :config (add-hook 'go-mode-hook #'go-eldoc-setup))
 
-  (use-package gorepl-mode
-    :ensure t
-    :diminish gorepl-mode
-    :config (add-hook 'go-mode-hook #'gorepl-mode)))
+(use-package company-go
+  :ensure t
+  :after go-mode
+  :config
+  (add-hook 'go-mode-hook 'company-mode)
+  (add-hook 'go-mode-hook (lambda ()
+                            (set (make-local-variable 'company-backends) '(company-go))
+                            (company-mode))))
+
+(use-package go-eldoc
+  :ensure t
+  :after go-mode
+  :diminish eldoc-mode
+  :init (add-hook 'go-mode-hook #'go-eldoc-setup))
+
+(use-package gorepl-mode
+  :ensure t
+  :after go-mode
+  :diminish gorepl-mode
+  :init (add-hook 'go-mode-hook #'gorepl-mode))
 
 (provide 'init-golang)
