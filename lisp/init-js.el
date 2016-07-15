5bf6dda4dccde6e0a2dd25c2cc42d4c897818e63
manute <manuel.alonso@protonmail.com>
2016-07-15 12:50:30 +0200

    adding skewer to js2-mode

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -9,4 +9,19 @@
   (setq flycheck-disable-checker '(javascript-jshint))
   (add-hook 'js2-mode-hook #'smartparens-mode))
 
+;; M-x run-skewer for start
+(use-package skewer-mode
+  :ensure t
+  :after js2-mode
+  :config
+  (add-hook 'js2-mode-hook #'skewer-mode))
+
+
+(use-package ac-js2
+  :ensure t
+  :after js2-mode
+  :config
+  (setq ac-js2-evaluate-calls t)
+  (add-hook 'js2-mode-hook #'ac-js2-mode))
+
 (provide 'init-js)
