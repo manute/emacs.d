c63e89b151db0c27c9022c778ea3d67d4c7f3996
manute <manuel.alonso@protonmail.com>
2016-07-15 11:32:37 +0200

    fixing elm mode

--- lisp/init-elm.el
+++ lisp/init-elm.el
@@ -1,18 +1,22 @@
-;; THANKS TO http://www.lambdacat.com/post-modern-emacs-setup-for-elm/
+;; THANKS TO
+;; http://www.lambdacat.com/post-modern-emacs-setup-for-elm/
+;; https://github.com/avdgaag/dotfiles/blob/master/home/.emacs.d/modules/ag-elm.el
 (use-package elm-mode
   :ensure t
   :mode ("\\.elm\\'" . elm-mode)
+  :init
+  (add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
+  (setq elm-format-on-save t)
+  (setq elm-indent-offset 4)
+  (setq elm-tags-on-save t)
   :config
-  (add-hook 'elm-mode-hook
-          (lambda ()
-            (setq company-backends '(company-elm)))))
+  (add-to-list 'company-backends 'company-elm))
 
 (use-package flycheck-elm
   :ensure t
   :after elm-mode
   :config
-  (add-hook 'flycheck-mode-hook 'flycheck-elm-setup))
-
+  (add-hook 'flycheck-mode-hook #'flycheck-elm-setup))
 
 
 (provide 'init-elm)
