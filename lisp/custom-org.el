5f86cf22883af1a8605e3d5b6766e5f85b1aef99
manute <manuelalonso@invisionapp.com>
2020-12-18 12:29:01 +0100

    setting better bindings for org-roam

--- lisp/custom-org.el
+++ lisp/custom-org.el
@@ -86,11 +86,11 @@
   :custom
   (org-roam-directory "~/org/roam")
   :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n g" . org-roam-graph))
+              (("C-c o l" . org-roam)
+               ("C-c o f" . org-roam-find-file)
+               ("C-c o g" . org-roam-graph))
               :map org-mode-map
-              (("C-c n i" . org-roam-insert))
-              (("C-c n I" . org-roam-insert-immediate))))
+              (("C-c o i" . org-roam-insert))
+              (("C-c o I" . org-roam-insert-immediate))))
 
 (provide 'custom-org)
